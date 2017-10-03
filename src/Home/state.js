7fbb1d93e800a5114c90c6ecbfdb8e6290b9629f
Phat Le <ltanphat@gmail.com>
2017-10-03 16:49:55 +0700

    Add user ids state

--- src/Home/state.js
+++ src/Home/state.js
@@ -1,12 +1,26 @@
-import { createAction } from 'redux-actions';
+import { combineReducers } from 'redux';
+import { createAction, handleActions } from 'redux-actions';
 import { Observable } from 'rxjs';
 import { map, toString } from 'lodash/fp';
 
 const FETCH_GITHUB_USER = "Home/FETCH_GITHUB_USER";
 const NOTIFY_MESSAGE = "Home/NOTIFY_MESSAGE";
+const ADD_USER_ID = "Home/ADD_USER_ID";
+const ADD_USER_IDS = "Home/ADD_USER_IDS";
 
 export const fetchGithubUserAction = createAction(FETCH_GITHUB_USER);
 export const notifyMessage = createAction(NOTIFY_MESSAGE);
+export const addUserIdAction = createAction(ADD_USER_ID);
+export const addUserIdsAction = createAction(ADD_USER_IDS);
+
+const userIds = handleActions({
+  [ADD_USER_ID] : (state, { payload }) => [...state, payload],
+  [ADD_USER_IDS] : (state, { payload }) => [...state, ...payload],
+}, []);
+
+const reducer = combineReducers({
+  userIds,
+});
 
 const ghSearchBase = "https://api.github.com/search/users?q=";
 const singaporeSearchUrl = `${ghSearchBase}location:singapore`;
@@ -35,3 +49,5 @@ export const fetchUserEpic = (action$, _store, { get, getDB }) => (
       })))
 		)
 );
+
+export default { home: reducer }
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import home from '../Home/state';
 import database from '../db/state';
 
 export const rootReducer = combineReducers({
   ...database,
+  ...home,
 });
 
 export default rootReducer;
