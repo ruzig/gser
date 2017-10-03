95c54b059c407c12cf9aca61e491b346ef42e52e
Phat Le <ltanphat@gmail.com>
2017-10-03 13:34:30 +0700

    Add actions to fetch and receive users

--- src/Home/state.js
+++ src/Home/state.js
@@ -1,7 +1,14 @@
 import { createAction, handleActions } from 'redux-actions';
 import { combineReducers } from 'redux';
 
+const FETCH_GITHUB_USER = "Home/FETCH_GITHUB_USER";
+const ADD_USER_IDS = "Home/ADD_USER_IDS";
+
+export const fetchGithubUserAction = createAction(FETCH_GITHUB_USER);
+export const addUserIdsAction = createAction(ADD_USER_IDS);
+
 const userIds = handleActions({
+  [ADD_USER_IDS]: (state, { payload }) => [...state, ...payload],
 }, []);
 
 const reducer = combineReducers({
